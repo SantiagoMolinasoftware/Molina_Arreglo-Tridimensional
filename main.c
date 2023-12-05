ec8106a932acae413c07c9fb4185a0086d6abb95
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2023-12-04 22:11:42 -0500

    1er commit: inicio del programa y solicitud de valores para el programa

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int filas, columnas, profundidad;
+
+    // Solicitar al usuario el tamanio de los valores
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &filas);
+
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &columnas);
+
+    printf("Ingrese la cantidad de dimension: ");
+    scanf("%d", &profundidad);
+}

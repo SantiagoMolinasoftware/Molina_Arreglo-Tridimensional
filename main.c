3fecdf5033947d276ef067f1aad4827c0d994e4c
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2024-01-17 23:13:08 -0500

    3er commit: Crear un arreglo de estructuras Alumno

--- main.c
+++ main.c
@@ -12,3 +12,15 @@ struct Alumno {
 // Prototipos de funciones
 void ingresarDatos(struct Alumno *alumnos, int n);
 void mostrarDatos(struct Alumno *alumnos, int n);
+
+int main() {
+    
+    int n;
+    printf("Ingrese el numero de alumnos: ");
+    scanf("%d", &n);
+
+    // Crear un arreglo de estructuras Alumno
+    struct Alumno alumnos[n];
+
+
+}

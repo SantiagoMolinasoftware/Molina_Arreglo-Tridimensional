348438d497d3d408a530bbd8657927e186a9163d
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2024-01-17 23:10:24 -0500

    1er commit: Definimos la estructura Alumno

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Definimos la estructura Alumno
+struct Alumno {
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char carrera[50];
+    float promedio;
+};
